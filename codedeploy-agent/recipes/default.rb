6873ec40d0cb1aa2512d574f8f26a2bde390e0d7
John Lodini <509761@gmelogin.com>
2016-05-02 11:32:58 -0500

    Update Codedeploy recipe

--- codedeploy-agent/recipes/default.rb
+++ codedeploy-agent/recipes/default.rb
@@ -1,6 +1,6 @@
 powershell_script 'deploy CodeDeploy' do
 code <<-EOH
-New-Item c:\temp PowerShell -type directory
+New-Item c:\temp -type directory
 Invoke-WebRequest -Uri https://s3-us-west-2.amazonaws.com/aws-codedeploy-us-west-2/latest/codedeploy-agent.msi -Outfile c:\temp\codedeployagent.msi
 c:\temp\codedeployagent.msi /quiet /l c:\temp\host-agent-install-log.txt
 $HTML = Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/instance-id
@@ -9,7 +9,7 @@ $regionHTML = Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/pla
 $region = $regionHTML.Content.Substring(0,$regionHTML.Content.Length-1)
 $tags=Get-EC2Tag -region us-west-2 -Filter @{name="key";Values="opsworks:stack"} | Where-Object {$_.ResourceId -eq $instanceId}
 Update-CDDeploymentGroup -region us-west-2 -ApplicationName $tags.Value -CurrentDeploymentGroupName $tags.Value
-Restart-Service -Name codedeployagent
+Restart-Service -Name codedeployagent -force
 EOH
 end
 
