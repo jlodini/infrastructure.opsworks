248f95dce69f6bb931f5170b989536073f085f2b
John Lodini <509761@gmelogin.com>
2016-05-02 15:59:55 -0500

    fixinging IIS

--- iis-cookbook/recipes/install.rb
+++ iis-cookbook/recipes/install.rb
@@ -1,8 +1,7 @@
 powershell_script "Linking Codedeploy" do
   code <<-EOH
-  
-   Import-Module ServerManager  
-Add-WindowsFeature -Name Web-Server,Web-WebServer,Web-Common-Http,Web-Default-Doc,Web-Http-Errors,Web-Static-Content,Web-Http-Redirect,Web-Health,Web-Http-Logging,Web-Custom-Logging,Web-Log-Libraries,Web-Request-Monitor,Web-Http-Tracing,Web-Performance,Web-Stat-Compression,Web-Dyn-Compression,Web-Security,Web-Filtering,Web-Basic-Auth,Web-Cert-Auth,Web-Windows-Auth,Web-App-Dev,Web-Net-Ext,Web-Net-Ext45,Web-Asp-Net,Web-Asp-Net45,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-Mgmt-Tools,Web-Mgmt-Console,Web-Mgmt-Compat,Web-Metabase,Web-Lgcy-Mgmt-Console,Web-Lgcy-Scripting,Web-WMI,Web-Scripting-Tools,Web-Mgmt-Service,Web-Digest-Auth,Web-CertProvider
+Import-Module ServerManager 
+Add-WindowsFeature -Name Web-Server
   
   
   EOH
